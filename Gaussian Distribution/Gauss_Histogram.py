4b73120965b33ebc3f67cdb29b51a2d4a7755426
Kyle Bowman <kylebowman99@gmail.com>
2017-07-02 17:44:32 -0400

    Added probabilities for a given range

--- Gaussian Distribution/Gauss_Histogram.py
+++ Gaussian Distribution/Gauss_Histogram.py
@@ -6,6 +6,21 @@ import matplotlib.pyplot as plt
 import plotly as py
 import csv
 
+
+def get_probability(rand_nums, range_list):
+    '''
+    Gets probability of a value occurring given a set of random values and ranges
+    :param rand_nums: List of random numbers
+    :param range_list: List of different values of ranges
+    :return: List of probabilities for a given range
+    '''
+    probabilities = []
+    for index, val in enumerate(range_list[:len(range_list) - 1]):
+        # List of probabilities for each range by summing total count in range and dividing by total count
+        range_count = sum(1 for i in rand_nums if val <= i <= range_list[index + 1])
+        probabilities.append(range_count/len(rand_nums))
+    return probabilities
+
 # Learn about API authentication here: https://plot.ly/python/getting-started
 # API setup https://plot.ly/settings/api
 py.tools.set_credentials_file(username='Username', api_key='APIKEY')
@@ -24,11 +39,6 @@ num_bins = 18
 ranges = [mu + value*sigma for value in range(-4, 5)]
 
 # To gather values
-with open('gauss_values.csv', 'w') as file:
-    writer = csv.writer(file)
-    writer.writerow([i for i in range(-4, 5)])
-    writer.writerow(ranges)
-
 g_rand = np.random.normal(mu, sigma, 5000)
 
 fig = plt.figure()
@@ -39,5 +49,13 @@ plt.plot(bins, y, 'r--')
 plt.xlabel('Probability')
 plt.ylabel('Value')
 
-plot_url = py.plotly.plot_mpl(fig, filename='histogram_gauss')
 
+with open('gauss_values.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([i for i in range(-4, 5)])
+    writer.writerow(ranges)
+    writer.writerow([])
+    writer.writerow(['-4 to -3', '-3 to -2', '-2 to -1', '-1 to 0', '0 to 1', '1 to 2', '2 to 3', '3 to 4'])
+    writer.writerow(get_probability(g_rand, ranges))
+
+plot_url = py.plotly.plot_mpl(fig, filename='histogram_gauss')
